df7415a9f43a2af4770879f4e2c062ad288622c1
Eniola Osabiya <eosabiya@gmail.com>
2021-11-16 18:12:55 +0100

    moved all configs to env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,19 @@
-const { initializeApp } = require("firebase/app");
 const adminSDK = require('firebase-admin');
-const firebaseCredentials = require("./todosay-firebase-sdk.json");
+const { initializeApp } = require("firebase/app");
 
 adminSDK.initializeApp({
-    credential: adminSDK.credential.cert(firebaseCredentials)
+    credential: adminSDK.credential.cert({
+        type: process.env.type,
+        project_id: process.env.project_id,
+        private_key_id: process.env.private_key_id,
+        private_key: process.env.private_key,
+        client_email: process.env.client_email,
+        client_id: process.env.client_id,
+        auth_uri: process.env.auth_uri,
+        token_uri: process.env.token_uri,
+        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url: process.env.client_x509_cert_url
+    }),
 });
 
 const userSDK = initializeApp({
